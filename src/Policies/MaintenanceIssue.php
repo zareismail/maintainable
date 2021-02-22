f81c832ce488633a494900282d4a7e5bbc8d9b82
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-02-22 09:02:47 +0330

    Added authorization to settle up an issue.

--- src/Policies/MaintenanceIssue.php
+++ src/Policies/MaintenanceIssue.php
@@ -5,4 +5,13 @@ namespace Zareismail\Maintainable\Policies;
 
 class MaintenanceIssue extends Policy
 { 
+    /**
+     * Authorize user to settle up an issue.
+     * 
+     * @return 
+     */
+    public function settleUp($user, $issue) 
+    {
+        return $user->isDeveloper(); 
+    }
 }
